18120dfcaf6625f8d1e1d4b3e5f13c29af23b1ff
Yao Chen <m15229204898@163.com>
2017-05-22 15:52:55 +0800

    commit

--- example/public/javascripts/mainCnotroller.js
+++ example/public/javascripts/mainCnotroller.js
@@ -1,7 +1,7 @@
 /**
  * Created by Yao on 8/4/2017.
  */
-angular.module('myApp',[])
+angular.module('myApp',['angularTrix'])
     .controller('mainController',['$scope',function($scope){
         $scope.click = function(){
             console.log("click..");
